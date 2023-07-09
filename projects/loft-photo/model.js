49e73cbf698b9c673812388d4bdedf648a48558c
Arsen Temerkhanov <arsentemerkhanov@yandex.ru>
2023-07-09 10:00:36 +0500

    loft-photo_week-2

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,22 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if(!array.length){
+      return null;
+    }
+
+    let random = parseInt(Math.random(array.length) * 10); 
+    
+    return array[random];
+  },
+  getNextPhoto() {
+
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB[friend.id]; 
+    const photo = this.getRandomElement(photos)
+    
+    return {friend, url : photo.url}
+
+  },
 };
